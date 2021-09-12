1a3610340fc3193666127712d9db71ad92566d9f
Reagan Brasch <reaganbrasch@gmail.com>
2021-09-12 14:40:41 +1000

    updated TimeManager.cs

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TimeManager : MonoBehaviour
-{ 
+{
+    int lastTime = 0;
 
-    
+    private void Update()
+    {
+        if ((int) Time.time >= lastTime)
+        {
+            Debug.Log(lastTime);
+            lastTime++;
+        }
+    }
 }
