09af9aeddc976e0d062d1238e71965d148778176
Reagan Brasch <reaganbrasch@gmail.com>
2021-09-12 15:01:55 +1000

    60% complete

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
-    int lastTime = 0;
+    int lastTime;
+    float timer;
 
-    private void Update()
+    void Start()
     {
-        if ((int) Time.time >= lastTime)
+        ResetTime();
+    }
+
+    void Update()
+    {
+        if ((int) timer >= lastTime)
         {
             Debug.Log(lastTime);
             lastTime++;
         }
+
+        timer += Time.deltaTime;
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Time.timeScale = (Time.timeScale == 1f ? 0f : 1f);
             Debug.Log("Spacebar pressed");
         }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ResetTime();
+        }
+    }
+
+    private void ResetTime()
+    {
+        timer = 0;
+        lastTime = 0;
     }
 }
